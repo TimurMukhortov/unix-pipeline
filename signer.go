49aca2a425e01c0a3a4efe480ede62577985e353
t.mukhortov <t.mukhortov@gmail.com>
2020-06-21 00:19:11 +0300

    Создать функции для тестов

--- signer.go
+++ signer.go
@@ -1,5 +1,21 @@
 package main
 
+func SingleHash(in chan interface{}, out chan interface{}) {
+
+}
+
+func MultiHash(in chan interface{}, out chan interface{}) {
+
+}
+
+func CombineResults(in chan interface{}, out chan interface{}) {
+
+}
+
+func ExecutePipeline(jobs ...job) {
+
+}
+
 func main() {
 
 }
