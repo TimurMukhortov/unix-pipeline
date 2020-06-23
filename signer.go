805b4add7994a8ee1fc1eb317728372511bd3902
t.mukhortov <t.mukhortov@gmail.com>
2020-06-23 23:15:12 +0300

    Last ver

--- signer.go
+++ signer.go
@@ -1,15 +1,60 @@
 package main
 
-func SingleHash(in chan interface{}, out chan interface{}) {
+import (
+	"sort"
+	"strconv"
+	"strings"
+	"sync"
+)
 
+func SingleHash(in chan interface{}, out chan interface{}) {
+	for input := range in {
+		dataInt, ok := input.(int)
+		if !ok {
+			panic("Could not convert data to int")
+		}
+		dataStr := strconv.Itoa(dataInt)
+		wg := sync.WaitGroup{}
+		wg.Add(2)
+		results := make([]string, 2)
+		go func() {
+			results[0] = DataSignerCrc32(dataStr)
+			wg.Done()
+		}()
+		go func() {
+			results[1] = DataSignerCrc32(DataSignerMd5(dataStr))
+			wg.Done()
+		}()
+		wg.Wait()
+		out <- results[0] + "~" + results[1]
+	}
 }
 
 func MultiHash(in chan interface{}, out chan interface{}) {
-
+	for input := range in {
+		dataStr, ok := input.(string)
+		if !ok {
+			panic("Could not convert data to string")
+		}
+		var resultString string
+		for number := 0; number < 6; number++ {
+			resultString += DataSignerCrc32(strconv.Itoa(number) + dataStr)
+		}
+		out <- resultString
+	}
 }
 
 func CombineResults(in chan interface{}, out chan interface{}) {
-
+	values := []string{}
+	for input := range in {
+		dataStr, ok := input.(string)
+		if !ok {
+			panic("Could not convert data to string")
+		}
+		values = append(values, dataStr)
+	}
+	sort.Strings(values)
+	out <- strings.Join(values, "_")
 }
 
 func ExecutePipeline(jobs ...job) {
