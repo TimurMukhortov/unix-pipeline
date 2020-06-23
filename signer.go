b2282753b6cea60fe2bd6fca2819360cfcd9ffb3
t.mukhortov <t.mukhortov@gmail.com>
2020-06-23 14:59:21 +0300

    Прохождение одного теста на TestPipeline

--- signer.go
+++ signer.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"runtime"
+)
+
 func SingleHash(in chan interface{}, out chan interface{}) {
 
 }
@@ -13,9 +17,15 @@ func CombineResults(in chan interface{}, out chan interface{}) {
 }
 
 func ExecutePipeline(jobs ...job) {
-
+	inCh := make(chan interface{})
+	outCh := make(chan interface{})
+	for _, job := range jobs {
+		go job(inCh, outCh)
+		inCh = outCh
+		outCh = make(chan interface{})
+		runtime.Gosched()
+	}
 }
 
 func main() {
-
 }
